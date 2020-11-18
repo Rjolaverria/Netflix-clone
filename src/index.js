c4b9c1ebd500d248a11a73231c865fb145a76ee1
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-18 17:34:46 -0500

    updated for deployment

--- src/index.js
+++ src/index.js
@@ -8,8 +8,24 @@ import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import firebaseConfig from './firebaseConfig';
 import FirebaseContext from './context/firebase';
+
+let firebaseConfig;
+if (process.env.NODE_ENV === 'development'){
+    firebaseConfig = require('./firebaseConfig').default
+} else if (process.env.NODE_ENV === 'production'){
+    firebaseConfig = {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+    }
+}
+
 firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
