fb2ccccaca41a0ce4e83c8e4340bc987373926eb
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-18 20:04:22 -0500

    update

--- src/index.js
+++ src/index.js
@@ -16,16 +16,16 @@ if (process.env.NODE_ENV === 'development'){
 } else if (process.env.NODE_ENV === 'production'){
     firebaseConfig = {
         apiKey: process.env.REACT_APP_API_KEY,
-        authDomain: process.env.AUTH_DOMAIN,
-        databaseURL: process.env.DATABASE_URL,
-        projectId: process.env.PROJECT_ID,
-        storageBucket: process.env.STORAGE_BUCKET,
-        messagingSenderId: process.env.MESSAGING_SENDER_ID,
-        appId: process.env.APP_ID,
-        measurementId: process.env.MEASUREMENT_ID,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+        databaseURL: process.env.REACT_APP_DATABASE_URL,
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_APP_ID,
+        measurementId: process.env.REACT_APP_MEASUREMENT_ID,
     }
 }
-console.log(process)
+console.log(process.env)
 
 firebase.initializeApp(firebaseConfig);
 
