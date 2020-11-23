d7f9ddef3c5af9c615111b46c60bf46f219f49c7
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-23 11:23:04 -0500

    Update env

--- src/index.js
+++ src/index.js
@@ -10,23 +10,16 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import FirebaseContext from './context/firebase';
 
-let firebaseConfig;
-if (process.env.NODE_ENV === 'development'){
-    firebaseConfig = require('./firebaseConfig').default
-} else if (process.env.NODE_ENV === 'production'){
-    firebaseConfig = {
-        apiKey: process.env.REACT_APP_API_KEY,
-        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-        databaseURL: process.env.REACT_APP_DATABASE_URL,
-        projectId: process.env.REACT_APP_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-        appId: process.env.REACT_APP_APP_ID,
-        measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-    };
-}
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
 
 ReactDOM.render(
     <React.StrictMode>
